8f8fac0bb353f1c88e8221f6fc49d12ade921763
muhammedsemri <muhammed.semri@gmail.com>
2018-10-10 14:46:10 +0300

    loading...

--- commands.go
+++ commands.go
@@ -20,7 +20,7 @@ var TitleCmd = &cobra.Command{
 	Aliases: []string{
 		"t",
 	},
-	Args: cobra.MinimumNArgs(1),
+	Args: cobra.MinimumNArgs(3),
 	Run: func(cmd *cobra.Command, args []string) {
 		nt.Title = args[0]
 		if args[1] == "content" || args[1] == "-c" && args[2] != ""{
